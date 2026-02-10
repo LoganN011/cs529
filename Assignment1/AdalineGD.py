8d2206954c60708632b2d1779cc3e3217e6d10d5
Logan Nunno <lnunno1@unm.edu>
2026-02-09 20:25:20 -0700

    Problem 1 done

--- Assignment1/AdalineGD.py
+++ Assignment1/AdalineGD.py
@@ -1,29 +1,32 @@
 import numpy as np
 
+
 class AdalineGD:
-    def __init__(self, eta=0.01, n_iter=50, random_state=1): 
-        self.eta = eta 
-        self.n_iter = n_iter 
+
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
         self.random_state = random_state
 
     def fit(self, X, y):
-        rgen = np.random.RandomState(self.random_state) 
-        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=X.shape[1]) 
-        self.b_ = np.float_(0.) 
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
         self.losses_ = []
 
-        for i in range(self.n_iter): 
-            net_input = self.net_input(X) 
-            output = self.activation(net_input) 
-            errors = (y - output) 
-            self.w_ += self.eta * 2.0 * X.T.dot(errors) / X.shape[0] 
-            self.b_ += self.eta * 2.0 * errors.mean() 
-            loss = (errors**2).mean() 
+
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_ += self.eta * 2.0 * np.insert(X, 0, 1, axis=1).T.dot(errors) / X.shape[0]
+            # self.b_ += self.eta * 2.0 * errors.mean()
+            loss = (errors ** 2).mean()
             self.losses_.append(loss)
         return self
 
     def net_input(self, X):
-        return np.dot(X, self.w_) + self.b_
+        return np.dot(np.insert(X, 0, 1, axis=1), self.w_)
 
     def activation(self, X):
         return X
