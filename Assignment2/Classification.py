dd359b79eab6fca8adfbc11caffb7e36755fb6c5
lnunno1 <lnunno1@unm.edu>
2026-02-23 17:27:34 -0700

    Working task 2

---
+++ Assignment2/Classification.py
@@ -0,0 +1,61 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+def plot_classification(X, y,test_idx=None):
+    markers = ('o', 's', '^', 'v', '<')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    unique_labels = np.unique(y)
+
+    for idx, cl in enumerate(unique_labels):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    c=colors[idx],
+                    marker=markers[idx],
+                    label=f'Class {cl}',
+                    edgecolor='black')
+    if test_idx:
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='none',
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100,
+                    label='Test set')
+
+def make_classification(d,n,u,random_state=None,return_line=False):
+    rgen = np.random.RandomState(random_state)
+    a = rgen.randn(d)
+
+    X = rgen.uniform(-u, u, (n, d))
+
+    projections = np.dot(X, a)
+    y = np.where(projections < 0, -1, 1)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.30, random_state=random_state
+    )
+    if return_line:
+        return X_train, X_test, y_train, y_test, a
+    else:
+        return X_train, X_test, y_train, y_test
+
+
+X_train, X_test, y_train, y_test,a = make_classification(d=2, n=100, u=10,random_state=1,return_line=True)
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+
+plot_classification(X_combined, y_combined,test_idx=range(len(X_train),len(X_combined)))
+x1_min, x1_max = X_combined[:, 0].min() - 1, X_combined[:, 0].max() + 1
+x1_values = np.array([x1_min, x1_max])
+x2_values = -(a[0] / a[1]) * x1_values
+plt.plot(x1_values, x2_values, 'k--', label='Decision Boundary')
+plt.ylim(X_combined[:, 1].min() - 1, X_combined[:, 1].max() + 1)
+plt.legend(loc='upper left')
+plt.show()
+
