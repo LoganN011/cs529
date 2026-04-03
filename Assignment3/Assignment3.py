fd525e0a78a395554bacdf8e22b04656736bf277
LoganN011 <155611873+LoganN011@users.noreply.github.com>
2026-04-02 18:58:18 -0600

    reformat and added code for logistic regression

--- Assignment3/Assignment3.py
+++ Assignment3/Assignment3.py
@@ -1,13 +1,18 @@
+import time
+
 import pandas as pd
 import re
 
 import torch
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from torch import nn
 import torch.nn.functional
 from torch.utils.data import TensorDataset, DataLoader
 
+torch.manual_seed(1)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -19,13 +24,14 @@ def preprocessor(text):
             ' '.join(emoticons).replace('-', ''))
     return text
 
+
 def getData():
     df = pd.read_csv('movie_data.csv')
 
     df['review'] = df['review'].apply(preprocessor)
 
     X_train, X_test, y_train, y_test = train_test_split(
-       df['review'], df['sentiment'], test_size=0.3, random_state=1)
+        df['review'], df['sentiment'], test_size=0.3, random_state=1)
 
     tfidf = TfidfVectorizer(strip_accents=None,
                             lowercase=True,
@@ -48,7 +54,7 @@ def getData():
 
 
 class BasicNN(nn.Module):
-    def __init__(self,input_dim):
+    def __init__(self, input_dim):
         super(BasicNN, self).__init__()
         self.fc1 = nn.Linear(input_dim, 512)
         self.bn1 = nn.BatchNorm1d(512)
@@ -66,70 +72,108 @@ class BasicNN(nn.Module):
         return x
 
 
+def test_logistic_regression(train_data, test_data):
+
+    X_train = train_data.tensors[0].numpy()
+    y_train = train_data.tensors[1].numpy().ravel()
+    X_test = test_data.tensors[0].numpy()
+    y_test = test_data.tensors[1].numpy().ravel()
+
+
+    lr_model = LogisticRegression(solver='liblinear',random_state=1)
+
+
+    start_time = time.time()
+    lr_model.fit(X_train, y_train)
+    end_time = time.time()
+
+    y_pred = lr_model.predict(X_test)
+    test_accuracy = accuracy_score(y_test, y_pred)
+
+    y_pred = lr_model.predict(X_train)
+    train_accuracy = accuracy_score(y_train, y_pred)
+
+    print(f'Training Accuracy: {test_accuracy* 100:.2f}%')
+    print(f"Test Accuracy: {test_accuracy * 100:.2f}%")
+    print(f"Logistic Regression Training Time: {end_time - start_time:.2f}s")
+
+
+
 def train_validate_model(model, train_data, test_data):
-   train_losses = []
-   val_losses = []
-   train_accuracies = []
-   val_accuracies = []
-   learning_rate = 0.00001
-   num_epochs = 5
-   criterion = torch.nn.MSELoss()
-   optimizer =torch.optim.Adam(model.parameters(), lr=learning_rate,weight_decay=1e-4)
-   train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
-   test_loader = DataLoader(test_data, batch_size=64, shuffle=False)
+    train_losses = []
+    val_losses = []
+    train_accuracies = []
+    val_accuracies = []
+    learning_rate = 0.00001
+    num_epochs = 5
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-2)
+    train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=64, shuffle=False)
+    total_time = 0
 
-   for epoch in range(num_epochs):
-       model.train()
-       running_loss = 0.0
-       correct = 0
-       total = 0
+    for epoch in range(num_epochs):
+        start_time = time.time()
+        model.train()
+        running_loss = 0.0
+        correct = 0
+        total = 0
 
-       for images, labels in train_loader:
-           images, labels = images.to(device), labels.to(device)
-           outputs = model(images)
-           loss = criterion(outputs, labels)
-           loss.backward()
-           optimizer.step()
+        for images, labels in train_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-           running_loss += loss.item()
-           predicted = (outputs > 0.5).float()
-           correct += (predicted.view(-1) == labels.view(-1)).sum().item()
-           total += labels.size(0)
+            running_loss += loss.item()
+            predicted = (outputs > 0.5).float()
+            correct += (predicted.view(-1) == labels.view(-1)).sum().item()
+            total += labels.size(0)
 
-           optimizer.zero_grad()
+            optimizer.zero_grad()
 
-       epoch_loss = running_loss / len(train_loader)
-       epoch_accuracy = 100 * correct / total
-       train_losses.append(epoch_loss)
-       train_accuracies.append(epoch_accuracy)
+        epoch_loss = running_loss / len(train_loader)
+        epoch_accuracy = 100 * correct / total
+        train_losses.append(epoch_loss)
+        train_accuracies.append(epoch_accuracy)
 
+        end_time = time.time()
+        total_time += end_time - start_time
+        model.eval()
+        test_loss = 0.0
+        correct = 0
+        total = 0
 
-       model.eval()
-       test_loss = 0.0
-       correct = 0
-       total = 0
+        with torch.no_grad():
+            for images, labels in test_loader:
+                images, labels = images.to(device), labels.to(device)
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+                test_loss += loss.item()
+                predicted = (outputs > 0.5).float()
+                correct += (predicted.view(-1) == labels.view(-1)).sum().item()
+                total += labels.size(0)
 
-       with torch.no_grad():
-           for images, labels in test_loader:
-               images, labels = images.to(device), labels.to(device)
-               outputs = model(images)
-               loss = criterion(outputs, labels)
-               test_loss += loss.item()
-               predicted = (outputs > 0.5).float()
-               correct += (predicted.view(-1) == labels.view(-1)).sum().item()
-               total += labels.size(0)
+        test_loss /= len(test_loader)
+        val_accuracy = 100 * correct / total
+        val_losses.append(test_loss)
+        val_accuracies.append(val_accuracy)
 
-       test_loss /= len(test_loader)
-       val_accuracy = 100 * correct / total
-       val_losses.append(test_loss)
-       val_accuracies.append(val_accuracy)
+        print(
+            f'Epoch [{epoch + 1}/{num_epochs}], Loss: {epoch_loss:.4f}, Accuracy: {epoch_accuracy:.2f}%, Val Loss: {test_loss:.4f}, Val Accuracy: {val_accuracy:.2f}%')
+    print(f'Total Training Time: {total_time:.2f}s')
+    return train_losses, val_losses, train_accuracies, val_accuracies
 
-       print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {epoch_loss:.4f}, Accuracy: {epoch_accuracy:.2f}%, Val Loss: {test_loss:.4f}, Val Accuracy: {val_accuracy:.2f}%')
-   return train_losses, val_losses, train_accuracies, val_accuracies
 
 if __name__ == "__main__":
     print(device)
     train_data, test_data = getData()
     model = BasicNN(train_data.tensors[0].shape[1]).to(device)
+    print('Testing for BasicNN\n')
     train_validate_model(model, train_data, test_data)
+
+    print('\nTesting for Logistic Regression\n')
+    test_logistic_regression(train_data, test_data)
+
 
