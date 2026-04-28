d3de4b865f543bb9978368fb5b2753df93161c35
lnunno1 <lnunno1@unm.edu>
2026-04-28 17:18:19 -0600

    added the Environment class

--- Final/Environment.py
+++ Final/Environment.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 def abstract_map(img_path, size = (40,40)):
@@ -30,8 +31,77 @@ def abstract_map(img_path, size = (40,40)):
 
     return abstract_img
 
+class Environment:
+    def __init__(self, grid, target = (39, 39)):
+        self.grid = grid
+        self.target = target
+        self.width, self.height = self.grid.shape
+        self.actions = {0:"up", 1:"down", 2:"right", 3:"left"}
+
+
+    def step(self, state, action, strategy = "S1"):
+        x,y = state
+        next_x, next_y = state
+
+        new_state = (next_x, next_y)
+
+        if action == 0:
+            next_y+=1
+        elif action == 1:
+            next_y-=1
+        elif action == 2:
+            next_x+=1
+        elif action == 3:
+            next_x-=1
+
+        hit = False
+        out_of_grid = not (0 <= next_x < self.height and 0 <= next_y < self.width)
+
+        if out_of_grid:
+            hit = True
+            new_state = (x, y)
+        elif self.grid[next_x, next_y] == 0:
+            hit = True
+            new_state = (x, y)
+
+        reward = self._get_reward(state,hit, strategy)
+
+        return new_state, reward # add more things here???
+
+    def _get_reward(self, state, hit, strategy):
+        if strategy == "S1":
+            if state == self.target:
+                return 100
+            if hit:
+                return -100
+            else:
+                return 0
+        elif strategy == "S2": #Todo make a better strategy for S2
+            if state == self.target:
+                return 100
+            if hit:
+                return -100
+            else:
+                return -1
+
+        return 0
+
+
+    def plot_map(self):
+        plt.imshow(self.grid, cmap='gray',extent=(0,self.width,0,self.height))
+        plt.scatter(self.target[1] +.5 , self.height - self.target[0]-.5 ,c='red', marker='*')
+        plt.title("Abstracted Map Environment")
+
+        plt.xticks(np.arange(-5, self.width + 6, 5))
+        plt.yticks(np.arange(-5, self.height + 6, 5))
+        plt.xticks(np.arange(0,self.width+1, 1), minor=True)
+        plt.yticks(np.arange(0,self.height+1, 1), minor=True)
+        plt.grid(which='both' , color='gray', linestyle='-', linewidth=0.2)
+
+        plt.show()
 
 
 if __name__ == '__main__':
-    test = abstract_map("./Input_Maps/map2.bmp")
-    print(test)
+    test = abstract_map("./Input_Maps/map4.bmp")
+    env = Environment(test)
+    env.plot_map()
